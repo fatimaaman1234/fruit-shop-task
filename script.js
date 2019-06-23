513b08d5f4dc02982716c4a7193157dd5aadacc6
fatimaaman1234 <49521217+fatimaaman1234@users.noreply.github.com>
2019-06-23 15:22:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let priceOfApple = 10;
+let priceOfOrange = 15;
+let priceOfBanana = 7;
+let noOfApple = 0;
+let noOfOrange = 0;
+let noOfBanana = 0;
+
+let pocket = (document.querySelector("#pocket").value);
+
+let totalAmount = (document.querySelector("#total").value);
+
+
+function plusApple(){
+    document.querySelector("#appleqnty").value = ++noOfApple;
+   
+}
+function minusApple(){
+    document.querySelector("#appleqnty").value = --noOfApple;
+}
+
+
+function plusOrange(){
+    document.querySelector("#orangeqnty").value = ++noOfOrange;
+}
+function minusOrange(){
+    document.querySelector("#orangeqnty").value = --noOfOrange;
+}
+
+
+function plusBanana(){
+    document.querySelector("#bananaqnty").value = ++noOfBanana;
+}
+function minusBanana(){
+    document.querySelector("#bananaqnty").value = --noOfBanana;
+}
+
+
+
+
+
+
